314590c687767ea92e444cc2fe1b7f29396f7a03
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-04-19 00:49:06 +0530

    update server.js

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send('API Running')
 })
 
-const PORT = process.env.PORT || 3000;
+// Define Routes
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/post', require('./routes/api/post'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/users', require('./routes/api/users'));
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
