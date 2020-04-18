f9d20b27f2be0fca7f5ee98fb81388777afcdd6a
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-04-19 01:09:31 +0530

    add get user api

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const auth = require('../../middleware/auth');
+
+const User = require("../../models/User");
 
 /**
  * @route   GET api/auth
  * @desc    Test auth
  * @access  Public
  */
-router.get("/", (req, res) => res.send('Auth route'));
+router.get("/", auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password').exec();
+        res.json(user);
+    } catch (error) {
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
