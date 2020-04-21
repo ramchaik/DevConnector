8f44160591f5dc8e90cccbba559654f47ffac9c2
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-04-22 00:58:35 +0530

    INFO: set up post model

---
+++ models/Post.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "user",
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    },
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+      text: {
+        type: String,
+      },
+      name: {
+        type: String,
+      },
+      avatar: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        default: Data.now,
+      },
+    },
+  ],
+  date: {
+    type: Date,
+    default: Data.now,
+  },
+});
+
+module.exports = mongoose.model('post', PostSchema);
